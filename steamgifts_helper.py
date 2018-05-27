dedea4296879f0abc647b908111fd487db908e23
Derek Enlow <theunnamed97@gmail.com>
2018-05-26 19:57:16 -0600

    giveaways now entered + already-entered giveaway handling

--- steamgifts_helper.py
+++ steamgifts_helper.py
@@ -1,7 +1,7 @@
 import pickle
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -22,7 +22,7 @@ with open("cookies/baked_cookies.pkl", "rb") as baked_cookies:
 driver.get("https://www.steamgifts.com/giveaways/search?type=wishlist")
 try:
     #select titles for each giveaway for use of their hyperlink
-    elements = driver.find_elements(By.XPATH, "/html/body/div[3]/div/div/div[2]/div[3]//a[@class='giveaway__heading__name']")
+    elements = driver.find_elements(By.XPATH, "/html/body/div[3]/div/div/div[2]/div[3]//div[@class='giveaway__row-inner-wrap']//a[@class='giveaway__heading__name']")
 
     #extract hyperlinks
     href_list = []
@@ -30,5 +30,10 @@ try:
         href_list.append(element.get_attribute("href"))
     for href in href_list:
         driver.get(href)
+        try:
+            driver.find_element(By.CLASS_NAME, "sidebar__entry-insert").click()
+        except ElementNotInteractableException as e:
+            pass
+
 finally:
     driver.quit()
