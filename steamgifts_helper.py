89859e3fb35c9e3563ede083456c37cc97087e75
Derek Enlow <theunnamed97@gmail.com>
2018-05-27 21:31:59 -0600

    added multi-page traversal

--- steamgifts_helper.py
+++ steamgifts_helper.py
@@ -2,6 +2,7 @@ import pickle
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
@@ -28,16 +29,26 @@ try:
     #select titles for each giveaway for use of their hyperlink
     elements = driver.find_elements(By.XPATH, "/html/body/div[3]/div/div/div[2]/div[3]//div[@class='giveaway__row-inner-wrap']//a[@class='giveaway__heading__name']")
 
-    #extract hyperlinks
-    href_list = []
-    for element in elements:
-        href_list.append(element.get_attribute("href"))
-    for href in href_list:
-        driver.get(href)
+    total_entered = 0
+    while(True):
+        #extract hyperlinks
+        href_list = []
+        for element in elements:
+            href_list.append(element.get_attribute("href"))
+        #try to enter each giveaway
+        for href in href_list:
+            driver.get(href)
+            try:
+                driver.find_element(By.CLASS_NAME, "sidebar__entry-insert").click()
+                total_entered += 1
+            except ElementNotInteractableException as e:
+                pass
         try:
-            driver.find_element(By.CLASS_NAME, "sidebar__entry-insert").click()
-        except ElementNotInteractableException as e:
-            pass
+            next_page = driver.find_element(By.XPATH, "//div[@class='pagination__navigation']/a[3]")
+            next_page.click()
+        except NoSuchElementException as e:
+            break
+    print(str(total_entered) + " entered.")
 
 finally:
     driver.quit()
