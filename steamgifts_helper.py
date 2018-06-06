b61f6ca187c96825ff5a25c2197e014a9e00a21a
Derek Enlow <theunnamed97@gmail.com>
2018-06-05 19:07:19 -0600

    cleanup and error handling for bad cookies

--- steamgifts_helper.py
+++ steamgifts_helper.py
@@ -11,21 +11,35 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.set_headless(headless=True)
 driver = webdriver.Firefox(firefox_options=options)
-print("Headless Firefox initialized.")
-
-root = "https://www.steamgifts.com"
-
-# navigate to steamgifts
-driver.get(root)
-
-#load cookies for login from pickled object
-with open("cookies/baked_cookies.pkl", "rb") as baked_cookies:
-    for cookie in pickle.load(baked_cookies):
-        driver.add_cookie(cookie)
-
-#navigate to wishlist
-driver.get("https://www.steamgifts.com/giveaways/search?type=wishlist")
 try:
+    print("Firefox driver initialized.")
+
+
+    # navigate to steamgifts
+    root = "https://www.steamgifts.com"
+    driver.get(root)
+
+
+    #load cookies for login from pickled object
+    print("Adding cookies...")
+    with open("cookies/baked_cookies.pkl", "rb") as baked_cookies:
+        for cookie in pickle.load(baked_cookies):
+            driver.add_cookie(cookie)
+    print("Cookies added.")
+
+
+    #navigate to wishlist
+    driver.get("https://www.steamgifts.com/giveaways/search?type=wishlist")
+    #assert login was loaded from Cookies
+    try:
+        driver.find_element(By.XPATH, "//a[@class='nav__avatar-outer-wrap']")
+    except NoSuchElementException as e:
+        print("Account not present.")
+        raise e
+    else:
+        print("Account loaded successfully.")
+
+
     #select titles for each giveaway for use of their hyperlink
     elements = driver.find_elements(By.XPATH, "/html/body/div[3]/div/div/div[2]/div[3]//div[@class='giveaway__row-inner-wrap']//a[@class='giveaway__heading__name']")
 
@@ -40,15 +54,21 @@ try:
             driver.get(href)
             try:
                 driver.find_element(By.CLASS_NAME, "sidebar__entry-insert").click()
-                total_entered += 1
             except ElementNotInteractableException as e:
                 pass
+            else:
+                total_entered += 1
         try:
             next_page = driver.find_element(By.XPATH, "//div[@class='pagination__navigation']/a[3]")
-            next_page.click()
         except NoSuchElementException as e:
             break
+        else:
+            next_page.click()
+
     print(str(total_entered) + " entered.")
 
+
+except Exception as e:
+    raise e
 finally:
     driver.quit()
