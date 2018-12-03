d180fbc6762d702154f4f31df7be4b1fc1fa7450
necrotoxin44 <theunnamed97@gmail.com>
2018-12-03 11:47:08 -0500

    simplified

--- cookies/cookie_baker.py
+++ cookies/cookie_baker.py
@@ -1,14 +1,14 @@
 import pickle
 from selenium import webdriver
 
-#create instance of firefox driver
+# create instance of firefox driver
 driver = webdriver.Firefox()
 
-#do logins and create cookies
+# do logins and create cookies
 
 input("Press enter when done preparing cookies: ")
 
-#pickle the cookies
+# pickle the cookies
 with open("baked_cookies.pkl", "wb") as cookie_file:
     pickle.dump(driver.get_cookies(), cookie_file)
 
--- steamgifts_helper.py
+++ steamgifts_helper.py
@@ -7,30 +7,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 
-#create instance of (headless) Firefox driver
+# create instance of (headless) Firefox driver
 options = Options()
-options.set_headless(headless=True)
+options.add_argument('-headless')
 driver = webdriver.Firefox(firefox_options=options)
 try:
     print("Firefox driver initialized.")
-
 
     # navigate to steamgifts
     root = "https://www.steamgifts.com"
     driver.get(root)
 
-
-    #load cookies for login from pickled object
+    # load cookies for login from pickled object
     print("Adding cookies...")
     with open("cookies/baked_cookies.pkl", "rb") as baked_cookies:
         for cookie in pickle.load(baked_cookies):
             driver.add_cookie(cookie)
     print("Cookies added.")
 
-
-    #navigate to wishlist
+    # navigate to wish-list
     driver.get("https://www.steamgifts.com/giveaways/search?type=wishlist")
-    #assert login was loaded from Cookies
+    # assert login was loaded from Cookies
     try:
         driver.find_element(By.XPATH, "//a[@class='nav__avatar-outer-wrap']")
     except NoSuchElementException as e:
@@ -39,17 +36,18 @@ try:
     else:
         print("Account loaded successfully.")
 
-
-    #select titles for each giveaway for use of their hyperlink
-    elements = driver.find_elements(By.XPATH, "/html/body/div[3]/div/div/div[2]/div[3]//div[@class='giveaway__row-inner-wrap']//a[@class='giveaway__heading__name']")
+    # select titles for each giveaway for use of their hyperlink
+    giveaway_path = ("/html/body/div[3]/div/div/div[2]/div[3]//div[@class='giveaway__row-inner-wrap']"
+                     "//a[@class='giveaway__heading__name']")
+    elements = driver.find_elements(By.XPATH, giveaway_path)
 
     total_entered = 0
-    while(True):
-        #extract hyperlinks
+    while True:
+        # extract hyperlinks
         href_list = []
         for element in elements:
             href_list.append(element.get_attribute("href"))
-        #try to enter each giveaway
+        # try to enter each giveaway
         for href in href_list:
             driver.get(href)
             try:
@@ -67,6 +65,9 @@ try:
 
     print(str(total_entered) + " entered.")
 
+    # Renew cookies
+    with open("cookies/baked_cookies.pkl", "wb") as cookies_to_bake:
+        pickle.dump(driver.get_cookies(), cookies_to_bake)
 
 except Exception as e:
     raise e
