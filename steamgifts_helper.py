ebbc6a4d4cb2bce47115bd4d9f42853505e1fd7f
necrotoxin44 <theunnamed97@gmail.com>
2019-08-15 16:27:20 -0600

    beginning of revamp

--- steamgifts_helper.py
+++ steamgifts_helper.py
@@ -7,69 +7,113 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 
-# create instance of (headless) Firefox driver
-options = Options()
-options.add_argument('-headless')
-driver = webdriver.Firefox(firefox_options=options)
-try:
-    print("Firefox driver initialized.")
 
-    # navigate to steamgifts
-    root = "https://www.steamgifts.com"
+root = 'https://www.steamgifts.com'
+source_dict = [
+    'all': 'https://www.steamgifts.com/',
+    'wishlist': 'https://www.steamgifts.com/giveaways/search?type=wishlist',
+    'recommended': 'https://www.steamgifts.com/giveaways/search?type=recommended',
+    'group': 'https://www.steamgifts.com/giveaways/search?type=group',
+    'new': 'https://www.steamgifts.com/giveaways/search?type=new'
+]
+trail_of_cookies = 'cookies/baked_cookies.pkl'
+
+
+class Giveaway:
+    """docstring for ."""
+    def __init__(self, href, end_time, cost):
+        self.href = href
+        self.end_time = end_time
+        self.cost = cost
+
+
+def load_last_cookies(driver, path):
+    # Navigate driver to steamgifts.
     driver.get(root)
 
-    # load cookies for login from pickled object
-    print("Adding cookies...")
-    with open("cookies/baked_cookies.pkl", "rb") as baked_cookies:
+    # Add cookies for login to the driver, from a pickled object.
+    with open(path, 'rb') as baked_cookies:
         for cookie in pickle.load(baked_cookies):
             driver.add_cookie(cookie)
-    print("Cookies added.")
 
-    # navigate to wish-list
-    driver.get("https://www.steamgifts.com/giveaways/search?type=wishlist")
-    # assert login was loaded from Cookies
+def initialize_driver():
+    # create instance of (headless) Firefox driver
+    options = Options()
+    options.add_argument('-headless')
+    return webdriver.Firefox(firefox_options=options)
+
+def check_login_success(driver):
+    # Reload the page.
+    driver.get(root)
+    # Assert login was loaded from cookies.
     try:
         driver.find_element(By.XPATH, "//a[@class='nav__avatar-outer-wrap']")
     except NoSuchElementException as e:
-        print("Account not present.")
-        raise e
+        return False
     else:
-        print("Account loaded successfully.")
+        return True
 
-    # select titles for each giveaway for use of their hyperlink
-    giveaway_path = ("/html/body/div[3]/div/div/div[2]/div[3]//div[@class='giveaway__row-inner-wrap']"
-                     "//a[@class='giveaway__heading__name']")
-    elements = driver.find_elements(By.XPATH, giveaway_path)
-
-    total_entered = 0
-    while True:
-        # extract hyperlinks
-        href_list = []
-        for element in elements:
-            href_list.append(element.get_attribute("href"))
-        # try to enter each giveaway
-        for href in href_list:
-            driver.get(href)
-            try:
-                driver.find_element(By.CLASS_NAME, "sidebar__entry-insert").click()
-            except ElementNotInteractableException as e:
-                pass
-            else:
-                total_entered += 1
-        try:
-            next_page = driver.find_element(By.XPATH, "//div[@class='pagination__navigation']/a[3]")
-        except NoSuchElementException as e:
-            break
-        else:
-            next_page.click()
-
-    print(str(total_entered) + " entered.")
-
-    # Renew cookies
-    with open("cookies/baked_cookies.pkl", "wb") as cookies_to_bake:
+def renew_cookies(driver, path):
+    with open(path, 'wb') as cookies_to_bake:
         pickle.dump(driver.get_cookies(), cookies_to_bake)
 
+
+try:
+    d = initialize_driver()
+    print('Firefox driver initialized.')
+
+    print('Adding cookies...')
+    load_last_cookies(d, trail_of_cookies)
+    print('Cookies added.')
+
+    if check_login_success(d):
+        print("Account loaded successfully.")
+    else:
+        print("Successful login not found.")
+        break
+
+    giveaways = []
+    while pages:
+        curr_page = pages.pop()
+        if curr_page in visited:
+            continue
+        else:
+            visited.append(curr_page)
+        d.get(curr_page)
+        # select titles for each giveaway for use of their hyperlink
+        giveaway_path = ("/html/body/div[3]/div/div/div[2]/div[3]//div[@class='giveaway__row-inner-wrap']"
+                         "//a[@class='giveaway__heading__name']")
+        elements = d.find_elements(By.XPATH, giveaway_path)
+
+        total_entered = 0
+        # extract hyperlinks
+        for element in elements:
+            href_list.append(element.get_attribute('href'))
+
+        next_page = ''
+        try:
+            next_page = d.find_element(By.XPATH, "//div[@class='pagination__navigation']/a[3]")
+        except NoSuchElementException as e:
+            pass
+        else:
+            pages.insert(0, next_page.get_attribute('href'))
+
+    # # try to enter each giveaway
+    # for href in href_list:
+    #     d.get(href)
+    #     try:
+    #         d.find_element(By.CLASS_NAME, "sidebar__entry-insert").click()
+    #     except (ElementNotInteractableException, NoSuchElementException) as e:
+    #         break
+    #     else:
+    #         total_entered += 1
+    #
+    # print(str(total_entered) + " entered.")
+
+    renew_cookies(d, trail_of_cookies)
+
 except Exception as e:
+    print(e)
     raise e
 finally:
-    driver.quit()
+    d.quit()
