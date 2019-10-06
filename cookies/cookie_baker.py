0b9e4c67a7cb97788b57f6f95c5fa80b0e647f44
necrotoxin44 <theunnamed97@gmail.com>
2019-10-05 21:32:02 -0400

    =Made cookie_baker more useable.

--- cookies/cookie_baker.py
+++ cookies/cookie_baker.py
@@ -1,15 +1,34 @@
+import argparse
+import os
 import pickle
 from selenium import webdriver
+import sys
 
-# create instance of firefox driver
-driver = webdriver.Firefox()
 
-# do logins and create cookies
+def bake_cookies(pickle_path=''):
+    with webdriver.Firefox() as driver:
+        input('Press enter when done preparing cookies: ')
+        pickle_path += 'baked_cookies.pkl'
+        # TODO: could make sure browser hasn't been prematurely closed
+        with open(pickle_path, 'wb') as cookie_file:
+            pickle.dump(driver.get_cookies(), cookie_file)
 
-input("Press enter when done preparing cookies: ")
+if __name__ == '__main__':
+    def dir_path(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise argparse.ArgumentTypeError(f'readable_dir:{path} is not a valid path')
+    parser = argparse.ArgumentParser(description='Pickles session cookies for use by the SteamGifts little helper. Yum!')
+    parser.add_argument(
+        '--dir',
+        dest='output_dir',
+        help='Path to the output directory where we will store our precious pickled cookies.',
+        type=dir_path
+        )
 
-# pickle the cookies
-with open("baked_cookies.pkl", "wb") as cookie_file:
-    pickle.dump(driver.get_cookies(), cookie_file)
-
-driver.quit()
+    parsed_args = parser.parse_args(sys.argv[1:])
+    if parsed_args.output_dir:
+        bake_cookies(pickle_path=parsed_args.output_dir)
+    else:
+        bake_cookies()
